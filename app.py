61370bf9cfc94f8e95aa1053d2255cf6acc4514e
Zihang Xiao <87538313+zihangxiao@users.noreply.github.com>
2024-05-12 15:58:47 -0700

    Update app.py

--- app.py
+++ app.py
@@ -163,7 +163,8 @@ if not selected_route.empty:
     # Create a DataFrame from the dictionary
     df = pd.DataFrame(data)
     df = df.reset_index(drop=True)
-
+    print(df.shape)
+    print(df.iloc[0])
     with open('xgb_classifier.pkl', 'rb') as file:
         xgb_classifier = pickle.load(file)
         
